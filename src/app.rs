ec41da97be35ca1ec7804385a5ff88132c32eff1
Roman <rl16@posteo.net>
2023-08-25 23:32:01 +0200

    setup extensions and styling

--- src/app.rs
+++ src/app.rs
@@ -8,24 +8,15 @@ pub fn App(cx: Scope) -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context(cx);
 
-    view! {
-        cx,
-
-        // injects a stylesheet into the document <head>
-        // id=leptos means cargo-leptos will hot-reload this stylesheet
+    view! { cx,
         <Stylesheet id="leptos" href="/pkg/chacha.css"/>
 
-        // sets the document title
         <Title text="Welcome to Leptos"/>
 
-        // content for this welcome page
         <Router fallback=|cx| {
             let mut outside_errors = Errors::default();
             outside_errors.insert_with_default_key(AppError::NotFound);
-            view! { cx,
-                <ErrorTemplate outside_errors/>
-            }
-            .into_view(cx)
+            view! { cx, <ErrorTemplate outside_errors/> }.into_view(cx)
         }>
             <main>
                 <Routes>
